fe23d2fe1b9dd1f35625e39707c5f6c66c06c08f
Stefan Wagner <mail.bompo@gmail.com>
2024-09-23 22:04:07 +0200

    Initial commit

---
+++ .env.local.tpl
@@ -0,0 +1,16 @@
+# Endpoint for the API gateway, set it to your instance in .env.local for development
+WANDELAPI_BASE_URL="{{ .InstanceInformation.NovaApiHost }}" # e.g. http://172.30.0.91
+
+# The cell on the service manager the robot pad will try to connect to
+CELL_ID="{{ .InstanceInformation.CellName }}"
+
+###
+# SECRETS
+#
+# These should never be defined in the .env file, only in the gitignored .env.local or 
+# in the environment variables of the deployment.
+###
+
+# For basic auth with the API
+NOVA_USERNAME="{{ .InstanceInformation.BasicAuth.Username }}"
+NOVA_PASSWORD="{{ .InstanceInformation.BasicAuth.Password }}"
---
+++ src/getWandelApi.ts
@@ -0,0 +1,37 @@
+import { NovaClient } from "@wandelbots/wandelbots-js"
+import { env } from "./runtimeEnv"
+
+let nova: NovaClient | null = null
+
+const getSecureUrl = (url: string): string => {
+    if (!url) {
+        return url;
+    }
+    return url.startsWith('http://') || url.startsWith('https://') 
+      ? url 
+      : url.includes('wandelbots.io') 
+        ? `https://${url}` 
+        : `http://${url}`;
+}
+
+export const getNovaClient = () => {
+  if (!nova) {
+    const secureWandelAPIBaseURL = getSecureUrl(env.WANDELAPI_BASE_URL || "");
+    console.log(secureWandelAPIBaseURL)
+    nova = new NovaClient({
+      instanceUrl:
+        typeof window !== "undefined"
+          ? new URL(secureWandelAPIBaseURL || "", window.location.origin).href
+          : secureWandelAPIBaseURL || "",
+      cellId: env.CELL_ID || "cell",
+      username: env.NOVA_USERNAME || "",
+      password: env.NOVA_PASSWORD || "",
+      baseOptions: {
+        // Time out after 30 seconds
+        timeout: 30000,
+      },
+    })
+  }
+
+  return nova
+}
