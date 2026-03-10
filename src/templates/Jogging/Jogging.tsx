891309e02231a4efca1b73a3aff8cc3b237c60b7
3m5. Marcin Szalomski <marcin.szalomski@partner.wandelbots.com>
2026-03-10 13:25:55 +0100

    RB-3115: ADD jogging stream component

--- src/templates/Jogging/Jogging.tsx
+++ src/templates/Jogging/Jogging.tsx
@@ -1,8 +1,9 @@
 import React from "react"
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { observer } from "mobx-react-lite"
 import { useActiveRobot } from "@/WandelAppContext"
 import { SafetyBar } from "@wandelbots/wandelbots-js-react-components"
+import { JoggingStateStream } from "@/templates/Jogging/JoggingStateStream"
 
 import { Jogging3DCanvas } from "./Jogging3DCanvas"
 import { JoggingUI } from "./JoggingUI"
@@ -48,6 +49,22 @@ export const Jogging = observer(() => {
           safetyState={activeRobot.controllerState.safety_state}
         />
       </Box>
+      <Typography variant="body2" color="white">
+        <Box
+          sx={{
+            display: "flex",
+            width: "70%",
+            position: "absolute",
+            padding: "0 12px",
+            left: 0,
+            bottom: "12px",
+            flexDirection: "row",
+            gap: "12px",
+          }}
+        >
+          <JoggingStateStream />
+        </Box>
+      </Typography>
     </Box>
   )
 })
---
+++ src/templates/Jogging/JoggingStateStream.tsx
@@ -0,0 +1,41 @@
+import { observer } from "mobx-react-lite"
+import { useMemo } from "react"
+import { Stack, type StackOwnProps } from "@mui/material"
+import { radiansToDegrees } from "@wandelbots/nova-js"
+import { useActiveRobot } from "@/WandelAppContext"
+
+const stackStyling: StackOwnProps = {
+   direction: "column",
+   justifyContent: "flex-start",
+   alignItems: "flex-start",
+   spacing: 0.5,
+   letterSpacing: 0.6,
+ }
+
+export const JoggingStateStream = observer(() => {
+  const activeRobot = useActiveRobot()
+
+  const axisConfig = useMemo(() => {
+    return activeRobot.rapidlyChangingMotionState.joint_position
+      .filter(item => item !== undefined)
+  }, [activeRobot.rapidlyChangingMotionState])
+
+  return [
+    <Stack key={"connection-status"} {...stackStyling}>
+      <span style={{ color: "#ffffff88" }}>Connection status:</span>
+      <span style={{ color: "#2affbf" }}>connected</span>
+    </Stack>,
+    <Stack key={"connected-with"} {...stackStyling}>
+      <span style={{ color: "#ffffff88" }}>Connected with:</span>
+      <span>{activeRobot.modelFromController}</span>
+      <span>{activeRobot.motionGroupId}</span>
+    </Stack>,
+    <Stack key={"axis-joints"} {...stackStyling}>
+      {axisConfig.map((joint, index) => [
+        <span key={index}>
+          Joint {index + 1}: {Math.round(radiansToDegrees(joint))}°
+        </span>,
+      ])}
+    </Stack>,
+  ]
+})
