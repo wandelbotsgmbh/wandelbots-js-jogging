ecbdb7327f654c0ae8f767a85b4e01106e421d43
Stefan Wagner <stefan.wagner@wandelbots.com>
2026-03-11 10:21:30 +0100

    fix: remove hardcoded "connected"

--- src/templates/Jogging/JoggingStateStream.tsx
+++ src/templates/Jogging/JoggingStateStream.tsx
@@ -21,23 +21,21 @@ export const JoggingStateStream = observer(() => {
       (item) => item !== undefined,
     )
 
-  return [
-    <Stack key={"connection-status"} {...stackStyling}>
-      <span style={{ color: "#ffffff88" }}>Connection status:</span>
-      <span style={{ color: "#2affbf" }}>connected</span>
-    </Stack>,
-    <Stack key={"connected-with"} {...stackStyling}>
-      <span style={{ color: "#ffffff88" }}>Connected with:</span>
-      <span>{activeRobot.modelFromController}</span>
-      <span>{activeRobot.motionGroupId}</span>
-    </Stack>,
-    <Stack key={"axis-joints"} {...stackStyling}>
-      <span style={{ color: "#ffffff88" }}>Joints information:</span>
-      {axisConfig.map((joint, index) => [
-        <span key={index}>
-          Joint {index + 1}: {Math.round(radiansToDegrees(joint))}°
-        </span>,
-      ])}
-    </Stack>,
-  ]
+  return (
+    <>
+      <Stack key="connected-with" {...stackStyling}>
+        <span style={{ color: "#ffffff88" }}>Connected with:</span>
+        <span>{activeRobot.modelFromController}</span>
+        <span>{activeRobot.motionGroupId}</span>
+      </Stack>
+      <Stack key="axis-joints" {...stackStyling}>
+        <span style={{ color: "#ffffff88" }}>Joints information:</span>
+        {axisConfig.map((joint, index) => (
+          <span key={index}>
+            Joint {index + 1}: {Math.round(radiansToDegrees(joint))}°
+          </span>
+        ))}
+      </Stack>
+    </>
+  )
 })
